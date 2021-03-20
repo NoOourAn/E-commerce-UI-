74ebac56ed040ec2ebb4995e05bbea7d28589451
NoOourAn <nouranaborwash@gmail.com>
2021-03-20 13:05:40 +0200

    updates to deploy to heroku

--- src/app/Services/orders.service.ts
+++ src/app/Services/orders.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -57,7 +58,7 @@ export class OrdersService {
        'access_token':localStorage.getItem('access_token')
      })
    }
-   return this.myClient.get(`http://localhost:3000/api/orders/`,httpOption);
+   return this.myClient.get(`${environment.api}/api/orders/`,httpOption);
  }
 
 
@@ -68,7 +69,7 @@ export class OrdersService {
        'access_token':localStorage.getItem('access_token')
      })
    }
-   return this.myClient.post(`http://localhost:3000/api/orders/`,newProduct,httpOption);
+   return this.myClient.post(`${environment.api}/api/orders/`,newProduct,httpOption);
  }
 
  // admin can modify order status
@@ -80,7 +81,7 @@ export class OrdersService {
      }),
      
    }
-   return this.myClient.patch(`http://localhost:3000/api/orders/${id}`,{"status":status},httpOption);
+   return this.myClient.patch(`${environment.api}/api/orders/${id}`,{"status":status},httpOption);
  }
  // user can delete order if it is pending
  deleteOrder(id){
@@ -91,6 +92,6 @@ export class OrdersService {
      }),
      
    }
-   return this.myClient.delete(`http://localhost:3000/api/orders/${id}`,httpOption);
+   return this.myClient.delete(`${environment.api}/api/orders/${id}`,httpOption);
  }
 }
