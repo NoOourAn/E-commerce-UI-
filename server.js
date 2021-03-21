32ce9dc3eaf05be63a75aec68212bedd430b0031
NoOourAn <nouranaborwash@gmail.com>
2021-03-21 13:47:58 +0200

    fixing 'import' paths

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express()
 const port = 4200
 
 //set up static files
-app.use(express.static('./dist/my-app')) 
+app.use(express.static('./dist/maketo-shopping-mall')) 
 
 //general path
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/my-app/'});
+    res.sendFile('index.html', {root: 'dist/maketo-shopping-mall/'});
 });
 
 app.listen(process.env.PORT || port)
