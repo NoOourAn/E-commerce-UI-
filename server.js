7ebf94a93c9ca03dc7b2c86bd7194e541cc07d7c
NoOourAn <nouranaborwash@gmail.com>
2021-03-21 13:34:29 +0200

    fixing 'import' paths

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express()
 const port = 4200
 
 //set up static files
-app.use(express.static('./dist/frontend')) 
+app.use(express.static('./dist/my-app')) 
 
 //general path
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/frontend/'});
+    res.sendFile('index.html', {root: 'dist/my-app/'});
 });
 
 app.listen(process.env.PORT || port)
